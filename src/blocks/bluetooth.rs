3a8d605b8409db581b1f78762d8fcef7ec749568
Andreas Rottmann <a.rottmann@gmx.at>
2020-01-31 20:44:15 +0100

    Update `dbus` dependency
    This is done with minimal changes by adapting the imports, as
    described in
    <https://github.com/diwic/dbus-rs/blob/master/dbus/changes-in-0.7.md>. A
    migration to the new APIs can be done separately.

--- src/blocks/bluetooth.rs
+++ src/blocks/bluetooth.rs
@@ -4,7 +4,7 @@ use std::time::{Duration, Instant};
 
 use crossbeam_channel::Sender;
 use dbus;
-use dbus::stdintf::org_freedesktop_dbus::{ObjectManager, Properties};
+use dbus::ffidisp::stdintf::org_freedesktop_dbus::{ObjectManager, Properties};
 use uuid::Uuid;
 
 use crate::blocks::{Block, ConfigBlock};
@@ -19,12 +19,12 @@ pub struct BluetoothDevice {
     pub path: String,
     pub icon: Option<String>,
     pub label: String,
-    con: dbus::Connection,
+    con: dbus::ffidisp::Connection,
 }
 
 impl BluetoothDevice {
     pub fn new(mac: String, label: Option<String>) -> Result<Self> {
-        let con = dbus::Connection::get_private(dbus::BusType::System)
+        let con = dbus::ffidisp::Connection::get_private(dbus::ffidisp::BusType::System)
             .block_error("bluetooth", "Failed to establish D-Bus connection.")?;
 
         // Bluez does not provide a convenient way to, say, list devices, so we
@@ -120,7 +120,7 @@ impl BluetoothDevice {
     pub fn monitor(&self, id: String, update_request: Sender<Task>) {
         let path = self.path.clone();
         thread::spawn(move || {
-            let con = dbus::Connection::get_private(dbus::BusType::System)
+            let con = dbus::ffidisp::Connection::get_private(dbus::ffidisp::BusType::System)
                 .expect("Failed to establish D-Bus connection.");
             let rule = format!(
                 "type='signal',\
